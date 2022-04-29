00e02a5415ed9eae7c0d1e18e91e19f30544ebb5
BRAVO68WEB <jbandyopadhayaya@gmail.com>
2022-04-29 06:41:44 +0530

    Dockerize the App

---
+++ frontend/components/footer/footer.data.js
@@ -0,0 +1,50 @@
+export default {
+  // widgets: [
+  //   {
+  //     id: 2,
+  //     iconSrc: Community,
+  //     altText: "Chat",
+  //     title: "Chat Communication",
+  //     description:
+  //       "You can always reach us via chat in Discord and talk to anyone about the project whenever you want. If you have any questions or ideas you can easily post them on the server and we will respond to them as soon as possible.",
+  //   },
+  //   {
+  //     id: 1,
+  //     iconSrc: Chat,
+  //     altText: "Community",
+  //     title: "Join the Project",
+  //     description:
+  //       "Are you looking for an innovative remote project for your portfolio or do you need an internship? Then you’ve come to the right place. Join our project and help us realize our vision.",
+  //   },
+  //   {
+  //     id: 3,
+  //     iconSrc: Github,
+  //     altText: "Github",
+  //     title: "Github Access",
+  //     description:
+  //       "You would work with the team in a strictly organized Git system and can upload your code directly to your own branch. An admin from our team will then review your code and you can then merge it with a pull request.",
+  //   },
+  // ],
+  menuItem: [
+    {
+      path: "/",
+      label: "Home",
+    },
+    {
+      path: "/",
+      label: "Legal Notice",
+    },
+    {
+      path: "/",
+      label: "Terms & Conditions",
+    },
+    {
+      path: "/",
+      label: "Privacy Policy",
+    },
+    {
+      path: "/careers",
+      label: "Careers",
+    },
+  ],
+};
